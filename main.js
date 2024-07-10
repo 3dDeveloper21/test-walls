6d79602e157c963a123c0b47c36e7aeaa95a83da
mertol-okcu <mertolokcu@gmail.com>
2024-07-10 18:07:44 +0300

    add line function

--- main.js
+++ main.js
@@ -1,30 +1,53 @@
-import './style.css'
+import { getLinePoints } from "./line";
+import "./style.css";
+import * as THREE from "three";
 
-import * as THREE from 'three';
-
-const webglCanvas = document.querySelector('.webgl');
+const webglCanvas = document.querySelector(".webgl");
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-const renderer = new THREE.WebGLRenderer({canvas: webglCanvas, antialias: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
+const renderer = new THREE.WebGLRenderer({
+  canvas: webglCanvas,
+  antialias: true,
+});
+renderer.setPixelRatio(2);
+renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.setAnimationLoop(loop);
 // document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+let point1 = new THREE.Vector2(-2, 0);
+let point2 = new THREE.Vector2(3, 3);
+let thickness = 0.1;
+
+const points = getLinePoints(point1, point2, thickness);
+
+const shape = new THREE.Shape();
+shape.moveTo(points[0].x, points[0].y);
+shape.lineTo(points[1].x, points[1].y); // Line to next point
+shape.lineTo(points[2].x, points[2].y); // Line to next point
+shape.lineTo(points[3].x, points[3].y); // Line to next point
+shape.lineTo(points[0].x, points[0].y);
+
+const geometry = new THREE.ShapeGeometry(shape);
+const material = new THREE.MeshBasicMaterial();
 
 camera.position.z = 5;
 
-function animate() {
+const line = new THREE.Mesh(geometry, material);
+scene.add(line);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-
+let lastTimeMs = 0;
+function loop(timeMs) {
+  const deltaTimeMs = timeMs - lastTimeMs;
+  lastTimeMs = timeMs;
+  point1.y += 0.01;
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
 }
-
+loop(0);
