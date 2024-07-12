dc70ed851a715cc65659e829236280c9b1e9945b
mertol-okcu <mertolokcu@gmail.com>
2024-07-13 02:37:11 +0300

    add mousemove and mousedown

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { draw, keypressed } from "./lib/draw";
+import { draw, keypressed, mousemove, mousedown } from "./lib/draw";
 
 const canvas = document.querySelector("canvas");
 
@@ -9,6 +9,12 @@ function resizeCanvas() {
 resizeCanvas();
 window.addEventListener("resize", resizeCanvas);
 window.addEventListener("keypress", (event) => keypressed(event.code));
+window.addEventListener("mousemove", (event) =>
+  mousemove(event.offsetX, event.offsetY)
+);
+window.addEventListener("mousedown", (event) =>
+  mousedown(event.offsetX, event.offsetY)
+);
 
 let lastFrameTime = 0;
 function loop(time) {
