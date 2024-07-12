5a559f3b146bb7e976d527f36b052b9e551e9c66
mertol-okcu <mertolokcu@gmail.com>
2024-07-12 19:04:52 +0300

    switch to pure canvas2d

--- main.js
+++ main.js
@@ -1,73 +1,22 @@
-import { getLinePoints } from "./line";
-import "./style.css";
-import * as THREE from "three";
+import { draw, keypressed } from "./lib/draw";
 
-const webglCanvas = document.querySelector(".webgl");
+const canvas = document.querySelector("canvas");
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
+window.addEventListener("keypress", (event) => keypressed(event.code));
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: webglCanvas,
-  antialias: true,
-});
-renderer.setPixelRatio(2);
-renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.setAnimationLoop(loop);
-// document.body.appendChild( renderer.domElement );
-
-let point1 = new THREE.Vector2(-2, 0);
-let point2 = new THREE.Vector2(3, 3);
-let thickness = 0.1;
-
-const points = getLinePoints(point1, point2, thickness);
-
-const shape = new THREE.Shape();
-shape.moveTo(points[0].x, points[0].y);
-shape.lineTo(points[1].x, points[1].y); // Line to next point
-shape.lineTo(points[2].x, points[2].y); // Line to next point
-shape.lineTo(points[3].x, points[3].y); // Line to next point
-shape.lineTo(points[0].x, points[0].y);
-
-const geometry = new THREE.ShapeGeometry(shape);
-const material = new THREE.MeshBasicMaterial();
-
-camera.position.z = 5;
-
-const line = new THREE.Mesh(geometry, material);
-scene.add(line);
-
-let lastTimeMs = 0;
-function loop(timeMs) {
-  const deltaTimeMs = timeMs - lastTimeMs;
-  lastTimeMs = timeMs;
-  point1.y += 0.01;
-
-  const points = getLinePoints(point1, point2, thickness);
-
-  scene.remove(line);
-
-  const shape = new THREE.Shape();
-  shape.moveTo(points[0].x, points[0].y);
-  shape.lineTo(points[1].x, points[1].y); // Line to next point
-  shape.lineTo(points[2].x, points[2].y); // Line to next point
-  shape.lineTo(points[3].x, points[3].y);
-  shape.lineTo(points[0].x, points[0].y);
-
-  const geometry = new THREE.ShapeGeometry(shape);
-  const material = new THREE.MeshBasicMaterial();
-
-  camera.position.z = 5;
-
-  const newLine = new THREE.Mesh(geometry, material);
-  scene.add(newLine);
-
-  renderer.render(scene, camera);
+let lastFrameTime = 0;
+function loop(time) {
+  const delta = time - lastFrameTime;
+  lastFrameTime = time;
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  draw(ctx, delta);
   window.requestAnimationFrame(loop);
 }
-loop(0);
+loop();
